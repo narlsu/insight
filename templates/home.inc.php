e32a4c4344bd391aad2363f9127c7a3745ab3b55
Zak Wells <wellszak@gmail.com>
2017-01-30 15:34:13 +1300

    need to fix the grid with padding like dribble

--- templates/home.inc.php
+++ templates/home.inc.php
@@ -1,3 +1,33 @@
 <h1>Home Page Contents</h1>
-<?php var_dump($travelContents) ?>
+
+	<div class="row small-up-1 medium-up-4 large-up-4 large-10 medium-10 small-10 columns travel-contain">
+		<div class="large-3 medium-6 small-12 columns travel-div">
+			<figure>
+				
+				<figcaption></figcaption>
+			</figure>
+		</div>
+
+		<div class="large-3 medium-6 small-12 columns travel-div">
+			<figure>
+
+				<figcaption></figcaption>
+			</figure>
+		</div>
+
+		<div class="large-3 medium-6 small-12 columns travel-div">
+			<figure>
+
+				<figcaption></figcaption>
+			</figure>
+		</div>
+
+		<div class="large-3 medium-6 small-12 columns travel-div">
+			<figure>
+
+				<figcaption></figcaption>
+			</figure>
+		</div>
+	</div>
+
 </div>
